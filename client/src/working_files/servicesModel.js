485e1fca9780804580c8aceba8e24af36aa8527b
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-03-04 22:52:02 -0500

    removed workign directory

--- client/src/working_files/servicesModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const serviceSchema = new mongoose.Schema({
-    servicetitle: {type: String, required: true},  
-    servicedesc: {type: String, required: true},
-    date: {type: Date, default: Date.now}      
-})
-
-module.exports = mongoose.model('service', serviceSchema)
