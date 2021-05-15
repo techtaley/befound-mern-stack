730e9a8d279109a9a85da620240b4e0fe5905ce9
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-05-14 20:36:15 -0400

    update logout

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,30 +1,30 @@
 // import React from 'react'
 // import React, { Component } from 'react'
-//import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 //import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css" 
 //import axios from 'axios'
 import '../../src/App.css'
-//import {Redirect } from 'react-router-dom'
+import {Redirect } from 'react-router-dom'
 
 //on submit valus from state stored in payload
 export default function Logout(){
-    // const [ redirect, setRedirect ] = useState(false)     
+    const [ redirect, setRedirect ] = useState(false)     
  
-    // useEffect(() => {
-    //     async function logoutUser(){
-    // //         //axios sends data to database from state
-    // //         await axios.get('http://localhost:4000/api/login', 
-    // //             {withCredentials: true}).then(res => {
-    // //         })
+    useEffect(() => {
+        async function logoutUser(){
+    //         //axios sends data to database from state
+    //         await axios.get('http://localhost:4000/api/login', 
+    //             {withCredentials: true}).then(res => {
+    //         })
 
-    //         setRedirect(true)
-    //      }
+            setRedirect(true)
+         }
         
-    //      logoutUser()
-    // })    
+         logoutUser()
+    })    
 
- // if(redirect) return <Redirect to="/app/login" />   
+ if(redirect) return <Redirect to="/app/login" />   
 
     return (
         <div className='container'>
